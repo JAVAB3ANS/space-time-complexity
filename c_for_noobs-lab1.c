b03dedea1dfa573ce4bb8b5cb5f2fb29e7c1eba3
Jason Vu <jvu@scu.edu>
2021-03-10 00:24:57 -0800

    Update c_for_noobs-lab1.c

--- c_for_noobs-lab1.c
+++ c_for_noobs-lab1.c
@@ -25,32 +25,3 @@ int main(void)
     }
     printf("Total Correct Answers:%d\n", c);
     printf("out of 10");
-=======
-#include <stdio.h>
-int main(void)
-{
-    srand ((int) time (NULL));
-    int d;
-    int q;
-    int a;
-    int c = 0;
-    int n = 0;
-    while (n <= 9){
-        d = rand () % 12 + 1;
-        q = rand () % 13;
-        printf("What's %d\n", d*q);
-        printf("Divided by %d\n",d);
-        scanf("%d",&a);
-        if (a == q){
-            printf("Bravo! You're a mathematician\n");
-            c++;
-            printf("Correct Answers %d\n", c);
-        }else{
-            printf("The answer is wrong\n");
-            printf("The correct answer is %d\n",q);
-        }
-        n++;
-    }
-    printf("Total Correct Answers:%d\n", c);
-    printf("out of 10");
-}
